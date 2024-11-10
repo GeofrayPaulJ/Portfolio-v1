cc838e3a659ccf23a7e93b0b121d189f44bc8c7a
Geofray Paul J <geofraypaul1223@gmail.com>
2024-11-10 23:58:57 +0530

    Update settings.py

--- PortfolioBackend/settings.py
+++ PortfolioBackend/settings.py
@@ -24,7 +24,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app','portfolio-v1-m6ul.onrender.com','portfolio-v1-m6ul.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
